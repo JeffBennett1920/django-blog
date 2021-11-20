68f84e83458814824c043f2417e59d948993af29
Jeff Bennett <jeffbennett@jeff-bennetts-imac-2.local>
2021-11-20 13:31:52 -0800

    changed blogging detail view from a function to a generic class based callable

--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.detail import DetailView # new
 
 # Create your views here.
 
@@ -21,11 +22,15 @@ def list_view(request):
     context = {'posts': posts}
     return render(request, 'blogging/list.html', context) # added blogging
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
